9d789820b2c6cabaf56c43124f050be6796765fd
miya <sirotumesirokuma@gmail.com>
2023-09-10 23:31:52 +0900

    done

---
+++ types.ts
@@ -0,0 +1,25 @@
+export interface Profile {
+  id : number;
+  bio: string;
+  profileImageUrl: string;
+  userId: number;
+  user: UserType;
+}
+
+
+export interface UserType {
+  id : number;
+  username: string;
+  email: string;
+  password: string;
+  posts: PostTypes[];
+  profile: Profile;
+}
+
+export interface PostTypes {
+  id : number;
+  content: string;
+  createdAt: string;
+  authorId: number;
+  author: UserType;
+}
